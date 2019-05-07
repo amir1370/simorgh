818110779fd8315226307047aca31ec72740a636
Amirhossein Taherijam <amirhoseintaherijam@gmail.com>
2019-05-07 14:07:03 +0430

    revising templates and statics

--- simorgh/simorgh/settings.py
+++ simorgh/simorgh/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'simorgh.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.dirname('templates')],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,3 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'assets')
+]
+
